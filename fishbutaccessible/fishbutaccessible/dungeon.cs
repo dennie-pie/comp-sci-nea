48ee9b1344e2a9655ab95562de0da1672d97e229
deniz <84850367+dennie-pie@users.noreply.github.com>
2021-11-15 12:21:07 +0000

    Update dungeon.cs

--- fishbutaccessible/fishbutaccessible/dungeon.cs
+++ fishbutaccessible/fishbutaccessible/dungeon.cs
@@ -10,27 +10,37 @@ namespace fish
     {
         private int x, y;
         private int[] loc;
-        private bool centre, hasRoom;
+        private bool hasRoom;
+        private string gen;
 
         public void setHR(bool hr) { hasRoom = hr; }
         public bool getHR() { return hasRoom; }
+        public string getGen() { return gen; }
+        public void setGen(string ng) { gen = ng; }
         public int[] getLoc()
         {
             return loc;
         }
-        public Cells(int X, int Y, bool cent)
+        public Cells(int X, int Y, string cent)
         {
             x = X;
             y = Y;
             loc = new int[2] { x, y };
-            centre = cent;
+            gen = cent;
+            hasRoom = false;
+        }
+        public Cells(int X, int Y)
+        {
+            x = X;
+            y = Y;
+            loc = new int[2] { x, y };
             hasRoom = false;
         }
     }
 
     class dungeon
     {
-        private Cells[,] dungeonMap = new Cells[8, 8];
+        private Cells[,] dungeonMap = new Cells[9, 9];
 
         public Cells[,] getMap()
         {
@@ -49,8 +59,8 @@ namespace fish
                 {
                     string loc;
                     loc = (Convert.ToString(y) + Convert.ToString(x));
-                    if (loc == "44") { Cells cell = new Cells(x, y, true); cell.setHR(true); }
-                    else { Cells cell = new Cells(x, y, false); }
+                    if (loc == "44") { Cells cell = new Cells(x, y, "centre"); cell.setHR(true); dungeonMap[x, y] = cell; }
+                    else { Cells cell = new Cells(x, y); dungeonMap[x, y] = cell; }
                 }
             }
 
@@ -64,11 +74,73 @@ namespace fish
             int drunkards = r.Next(2, 4);
             for (int i = 0; i <= drunkards; i++)
             {
-                drunkard dr = new drunkard(4,4);
+                drunkard dr = new drunkard(4,4,i+1);
                 List<int[]> tempSteps = dr.walk();
                 foreach(var j in tempSteps)
                 {
+                    Console.WriteLine($"{j[0]},{j[1]}");
                     dungeonMap[j[0], j[1]].setHR(true);
+                    dungeonMap[j[0], j[1]].setGen(Convert.ToString(j[2]));
+                }
+                for (int l = 0; l < 9; l++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 9; j++)
+                    {
+                        switch (dungeonMap[l, j].getGen())
+                        {
+                            case "centre":
+                                Console.Write("[*]");
+                                break;
+                            case "1":
+                                Console.Write("[1]");
+                                break;
+                            case "2":
+                                Console.Write("[2]");
+                                break;
+                            case "3":
+                                Console.Write("[3]");
+                                break;
+                            case "4":
+                                Console.Write("[4]");
+                                break;
+                            case null:
+                                Console.Write("| |");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void tempDrawMap()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 9; j++)
+                {
+                    switch (dungeonMap[i, j].getGen())
+                    {
+                        case "centre":
+                            Console.Write("[*]");
+                            break;
+                        case "1":
+                            Console.Write("[1]");
+                            break;
+                        case "2":
+                            Console.Write("[2]");
+                            break;
+                        case "3":
+                            Console.Write("[3]");
+                            break;
+                        case "4":
+                            Console.Write("[4]");
+                            break;
+                        case null:
+                            Console.Write("| |");
+                            break;
+                    }
                 }
             }
         }
